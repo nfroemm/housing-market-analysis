99284698d1085381a68adbc7b769ad5a37ae9037
Nathan S. Froemming <natfro@gmail.com>
2021-08-28 12:52:13 -0500

    Adding file `run.sh` for running dashboard application from the command line

---
+++ run.sh
@@ -0,0 +1,4 @@
+# The command below allows you to launch the web-based dashboard application
+# from a Terminal command line.  NB: The environment variable 'MAPBOX_API_KEY'
+# MUST be properly set!  See `housing-market-dashboard.ipynb` for details.
+panel serve housing-market-dashboard.ipynb
